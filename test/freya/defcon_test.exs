e2c7a3d25a4ad33ac6dad46aa7418368731fadbd
mccraigmccraig <mccraigmccraig@gmail.com>
2025-09-22 13:44:55 +0100

    function composition with defcon

--- test/freya/defcon_test.exs
+++ test/freya/defcon_test.exs
@@ -13,8 +13,8 @@ defmodule Freya.DefconTest do
     end
 
     Freer.defconp write_and_sum(a, b), [Writer] do
-      put(a)
-      put(b)
+      _ <- put(a)
+      _ <- put(b)
       return(a + b)
     end
 
@@ -24,12 +24,24 @@ defmodule Freya.DefconTest do
 
     Freer.defcon safe_div(a, b), [Error] do
       if b == 0 do
-        throw_fx(:zero)
+        Error.throw_fx(:zero)
       else
         return(a / b)
       end
     else
       :zero -> return(:infty)
+    end
+
+    Freer.defcon sum_and_log(a, b), [Reader, Writer] do
+      s <- DefconExample.sum_env(a, b)
+      _ <- Writer.put({:sum, s})
+      return(s)
+    end
+
+    Freer.defcon sum_twice(a, b), [Reader] do
+      x <- DefconExample.sum_env(a, b)
+      y <- DefconExample.sum_env(x, 0)
+      return(y)
     end
   end
 
@@ -51,5 +63,31 @@ defmodule Freya.DefconTest do
 
     out2 = DefconExample.safe_div(10, 2) |> ErrorHandler.interpret_error() |> Freer.run()
     assert %Freya.RunOutcome{result: %Freya.Freer.OkResult{value: 5.0}} = out2
+  end
+end
+
+defmodule Freya.DefconCompositionTest do
+  use ExUnit.Case
+  alias Freya.Freer
+  alias Freya.Effects.{ReaderHandler, WriterHandler}
+
+  test "defcon composition: sum_and_log composes Reader and Writer and calls another defcon" do
+    out =
+      Freya.DefconTest.DefconExample.sum_and_log(1, 2)
+      |> ReaderHandler.interpret_reader(3)
+      |> WriterHandler.interpret_writer()
+      |> Freer.run()
+
+    assert %Freya.RunOutcome{result: %Freya.Freer.OkResult{value: 6}, outputs: %{writer: [{:sum, 6}]}} = out
+  end
+
+  test "defcon composition: sum_twice calls another defcon twice" do
+    out =
+      Freya.DefconTest.DefconExample.sum_twice(1, 2)
+      |> ReaderHandler.interpret_reader(3)
+      |> Freer.run()
+
+    # First sum_env: 1+2+3 = 6; second: 6+0+3 = 9
+    assert %Freya.RunOutcome{result: %Freya.Freer.OkResult{value: 9}} = out
   end
 end
