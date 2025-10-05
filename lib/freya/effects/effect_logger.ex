c74ddd7251c774332814c3f64e947874b2626fdd
mccraigmccraig <mccraigmccraig@gmail.com>
2025-10-05 13:00:52 +0100

    extend EffectLogger to scoped effects

--- lib/freya/effects/effect_logger.ex
+++ lib/freya/effects/effect_logger.ex
@@ -13,9 +13,8 @@ defmodule Freya.Effects.EffectLogger do
   use Freya.Freer.Ops, constructors: Freya.Effects.EffectLogger.EffectLoggerConstructors
 end
 
-defmodule Freya.Effects.EffectLogger.EffectLogEntry do
+defmodule Freya.Effects.EffectLogger.IntermediateEffectLogEntry do
   defstruct sig: nil, data: nil
-
   @type t :: %__MODULE__{sig: any, data: any}
 
   def new(sig, data) do
@@ -23,15 +22,31 @@ defmodule Freya.Effects.EffectLogger.EffectLogEntry do
   end
 end
 
+defmodule Freya.Effects.EffectLogger.EffectLogEntry do
+  alias Freya.Effects.EffectLogger.IntermediateEffectLogEntry
+
+  defstruct effects: []
+
+  @type t :: %__MODULE__{effects: list(IntermediateEffectLogEntry.t())}
+
+  def new(sig, data) do
+    %__MODULE__{effects: [IntermediateEffectLogEntry.new(sig, data)]}
+  end
+
+  def push_effect(self, sig, data) do
+    %{self | effects: [IntermediateEffectLogEntry.new(sig, data) | self.effects]}
+  end
+end
+
 defmodule Freya.Effects.EffectLogger.InterpretedEffectLogEntry do
   alias Freya.Effects.EffectLogger.EffectLogEntry
 
-  defstruct sig: nil, data: nil, value: nil
+  defstruct effects: [], value: nil
 
-  @type t :: %__MODULE__{sig: any, data: any, value: any}
+  @type t :: %__MODULE__{effects: list(IntermediateEffectLogEntry.t()), value: any}
 
   def set_value(%EffectLogEntry{} = log_entry, value) do
-    %__MODULE__{sig: log_entry.sig, data: log_entry.data, value: value}
+    %__MODULE__{effects: log_entry.effects, value: value}
   end
 end
 
@@ -64,6 +79,17 @@ defmodule Freya.Effects.EffectLogger.Log do
       _ ->
         raise ArgumentError,
           message: "unexpected effect: #{inspect(%{sig: sig, data: data}, pretty: true)}"
+    end
+  end
+
+  def push_effect(%__MODULE__{} = log, %Impure{sig: sig, data: data}) do
+    case log.queue do
+      [] ->
+        raise ArgumentError,
+          message: "unexpected effect: #{inspect(%{sig: sig, data: data}, pretty: true)}"
+
+      [%EffectLogEntry{effects: [_prev | _]} = le | rest] ->
+        %{log | queue: [EffectLogEntry.push_effect(le, sig, data) | rest]}
     end
   end
 
@@ -124,6 +150,8 @@ defmodule Freya.Effects.EffectLogger.Handler do
   alias Freya.Freer.Impure
   alias Freya.Effects.EffectLogger
   alias Freya.Effects.EffectLogger.Log
+  alias Freya.Effects.EffectLogger.EffectLogEntry
+  alias Freya.Effects.EffectLogger.IntermediateEffectLogEntry
   alias Freya.Effects.EffectLogger.LogInterpretedEffectValue
   alias Freya.Effects.EffectLogger.InterpretedEffectLogEntry
   alias Freya.Run.RunState
@@ -209,16 +237,22 @@ defmodule Freya.Effects.EffectLogger.Handler do
   def log_or_resume(%Impure{sig: sig, data: u, q: q} = computation, %Log{} = log) do
     case log.queue do
       [] ->
-        # unseen com[utation]
+        # unseen computation - log and carry on
         updated_log = Log.log_effect(log, computation)
         capture_k = fn v -> EffectLogger.log_interpreted_effect_value(v) end
         updated_q = q |> Freya.Freer.Impl.q_prepend(capture_k)
         {%Freer.Impure{sig: sig, data: u, q: updated_q}, updated_log}
 
+      # fully interpreted computation - we have a value
       [
         %InterpretedEffectLogEntry{
-          sig: log_entry_sig,
-          data: log_entry_data,
+          effects: [
+            %IntermediateEffectLogEntry{
+              sig: log_entry_sig,
+              data: log_entry_data
+            }
+            | _
+          ],
           value: value
         } = _log_entry
         | _rest
@@ -227,6 +261,18 @@ defmodule Freya.Effects.EffectLogger.Handler do
         # resumed computation
         updated_log = Log.consume_log_entry(log)
         {Freya.Freer.Impl.q_apply(q, value), updated_log}
+
+      # partially interpreted computation
+      [
+        %EffectLogEntry{
+          effects: [%IntermediateEffectLogEntry{} | _]
+        } = _log_entry
+        | _rest
+      ] ->
+        # push the ne effect tp the current log entry
+        # and carry on
+        updated_log = Log.push_effect(log, computation)
+        {computation, updated_log}
 
       _ ->
         raise ArgumentError,
