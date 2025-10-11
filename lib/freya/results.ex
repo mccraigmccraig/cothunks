c18bae7907857bcf8e7333c9e8452f0500baf8ae
mccraigmccraig <mccraigmccraig@gmail.com>
2025-10-11 10:38:14 +0100

    simplify

--- lib/freya/results.ex
+++ lib/freya/results.ex
@@ -1,3 +1,16 @@
+defprotocol Freya.Protocols.Result do
+  @fallback_to_any true
+
+  @spec type(t) :: atom
+  def type(result)
+
+  @spec value(t) :: any
+  def value(result)
+
+  @spec short_circuits?(t) :: boolean
+  def short_circuits?(result)
+end
+
 defmodule Freya.OkResult do
   @moduledoc """
   Result type for a finished computation
